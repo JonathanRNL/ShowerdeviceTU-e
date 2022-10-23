6961f71406e5a57dc7555f2331af2ce3a0b84627
ASAhmed2 <115955830+ASAhmed2@users.noreply.github.com>
2022-10-23 23:25:45 +0200

    Fixed flow, not that many necessary changes to main
    Flow now works with volume and switch, timer did not want to work so for now only this is possible. if water flow stops volume resets.

--- flow.py
+++ flow.py
@@ -1,10 +1,11 @@
 from machine import Pin, Timer
 import time
 
-
 class FlowMeter:
     def __init__(self):
         self.counter_pin = Pin(35, Pin.IN)
+        self.switch = Pin(02, Pin.IN)
+        self.volume = 0
         self.counter = 0
         self.tim = Timer(1)
         def mycallback(t):
@@ -17,6 +18,12 @@ class FlowMeter:
         
     def start(self):
         self.counter_pin.irq(trigger=Pin.IRQ_RISING, handler=self.interrupt_handler)
+        self.switch.irq(trigger=Pin.IRQ_FALLING, handler=self.reset)
+    def reset(self, pin):
+        self.counter = 0
+        self.volume = 0
         
     def readFlow(self) -> int:
-        return self.counter
+        previousvolume= self.volume
+        self.volume = (self.counter/11)/60 + previousvolume
+        return self.volume
