838b4268863185c69942e73946903076402daa27
JonathanRNL <99552363+JonathanRNL@users.noreply.github.com>
2022-10-25 23:39:14 +0200

    Create storage.py

---
+++ storage.py
@@ -0,0 +1,7 @@
+def write_file(filename:str, variable:str):
+    with open(filename, 'w') as my_file:
+        my_file.write(str(variable))
+        
+def read_file(filename:str):
+    with open(filename) as my_file:
+        return my_file.read()        
