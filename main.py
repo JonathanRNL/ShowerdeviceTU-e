a970f5541f104c08ec8647c918438bf8270d279f
JonathanRNL <99552363+JonathanRNL@users.noreply.github.com>
2022-10-26 14:10:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,23 +43,24 @@ while True:
         temp_sum += temp
         temp_average = temp_sum/n_counter
         storage.write_file("temperature.txt", temp_average) 
-        volume = int(flow_meter.readFlow())
+        volume = flow_meter.readFlow()
+        volume_rounded = int(flow_meter.readFlow())
         volume_2 = flow_meter.readFlow_2()
         storage.write_file("volume.txt", volume) 
         time_stamp = time.time()
-        if volume_2 > 0.005: #flow treshold
+        if volume_2 > 0.005: 3 #flow treshold
             n_counter_2 += 3.5 #change if needed due to lag
             duration:str = convert(n_counter_2)
             storage.write_file("timer.txt", duration) 
         duration_read_storage = storage.read_file("timer.txt")
         display.clear()
-        display.text(f'Water: {volume} liters', 10)
-        display.text(f'Degrees: {temp_rounded}', 25)
-        display.text(f'Time: {duration_read_storage}', 50)
+        display.text(f'Water:{volume_rounded} liters', 10)
+        display.text(f'Degrees:{temp_rounded}', 25)
+        display.text(f'Time:{duration_read_storage}', 50)
         display.show()
         #print(f'average temp {temp_average}')
         #print(f'time:{duration}')
-        #print(f'volume: {volume_2}')      
+        print(f'volume: {volume_2}')      
         cloud.sendData({ 'timestamp': time_stamp, 'temp': temp_average, 'volume': volume})        
     except Exception as ex:
         print('Exception', str(ex))
